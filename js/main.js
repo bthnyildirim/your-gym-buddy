261a73c1c4646ec68d31742e86c8cb362b0f856d
Batuhan Yildirim <bthn.yildirim@gmail.com>
2024-10-13 21:37:43 +0200

    create divs,add title pic,implement positions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-function player() {}
+class Player {
+  constructor() {
+    this.positionY = 0;
+    this.positionX = 0;
+    this.width = 20;
+    this.height = 20;
+
+    const domElement = document.createElement("div");
+    domElement.id = "player";
+    domElement.style.width = this.width + "px";
+    domElement.style.height = this.height + "px";
+    domElement.style.left = this.positionX + "px";
+    domElement.style.bottom = this.positionY + "px";
+
+    const board = document.getElementById("board");
+    board.appendChild(domElement);
+  }
+
+  moveLeft() {
+    this.positionY--;
+    domElement.style.bottom = this.positionY + "px";
+  }
+
+  moveRight() {
+    this.positionX++;
+    domElement.style.left = this.positionX + "px";
+  }
+}
+const player = new Player();
+player.moveLeft();
+player.moveLeft();
+player.moveLeft();
