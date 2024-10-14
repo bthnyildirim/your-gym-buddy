a9e65b8454fb8cf48cacec8f3fd9191025b62e0f
Batuhan Yildirim <bthn.yildirim@gmail.com>
2024-10-14 16:01:54 +0200

    positioning of div elements and random div  generator

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 class Player {
   constructor() {
-    this.positionY = 20;
-    this.positionX = 20;
+    this.positionY = 0;
+    this.positionX = 0;
     this.width = 40;
     this.height = 40;
 
@@ -17,16 +17,68 @@ class Player {
   }
 
   moveLeft() {
-    this.positionX--;
+    this.positionX -= 20;
     this.domElement.style.left = this.positionX + "px";
   }
 
   moveRight() {
-    this.positionX++;
+    this.positionX += 20;
     this.domElement.style.left = this.positionX + "px";
   }
 }
+
+class Obstacle1 {
+  constructor() {
+    this.width = 40;
+    this.positionY = 900;
+    this.positionX = 500 - this.width / 2;
+    this.width = 40;
+    this.height = 20;
+
+    // Generate a random positionX to place obstacles next to each other
+    this.positionX = Math.random() * (boardWidth - this.width);
+
+    this.domElement = document.createElement("div");
+    this.domElement.id = "Obstacle1";
+    this.domElement.style.width = this.width + "px";
+    this.domElement.style.height = this.height + "px";
+    this.domElement.style.left = this.positionX + "px";
+    this.domElement.style.bottom = this.positionY + "px";
+    this.domElement.style.position = "absolute";
+
+    const board = document.getElementById("board");
+    board.appendChild(this.domElement);
+  }
+  moveDown() {
+    this.positionY -= 25;
+    this.domElement.style.bottom = this.positionY + "px";
+  }
+}
+
 const player = new Player();
+
+// Array to store all obstacles
+const obstacles = [];
+const obstacleWidth = 40; // Define the width of each obstacle
+const boardWidth = 1800;
+
+setInterval(() => {
+  const newObstacle = new Obstacle1(); // Create a new obstacle
+  obstacles.push(newObstacle); // Add it to the array
+}, 1000);
+
+setInterval(() => {
+  obstacles.forEach((obstacle, index) => {
+    obstacle.moveDown();
+
+    // If the obstacle moves off the screen, remove it
+    if (obstacle.positionY < 0) {
+      obstacle.domElement.remove();
+      obstacles.splice(index, 1);
+    }
+  });
+}, 200);
+
 player.moveLeft();
 player.moveRight();
 document.addEventListener("keydown", (action) => {
